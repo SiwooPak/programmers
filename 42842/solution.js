6e970196e35781410fd6335301934e78ba9cc200
SiwooPak <bogus1620@naver.com>
2022-01-05 23:51:13 +0900

    Create solution.js

---
+++ 42842/solution.js
@@ -0,0 +1,17 @@
+function solution(brown, yellow) {
+    let answer = [0,0];
+    let total = brown+yellow;
+
+    for(let i=3; i<total/2; i++) {
+        let len = total/i;
+
+        if((i-2)*(len-2) === yellow) {
+            let weight = Math.max(len,i);
+            let height = Math.min(len,i);
+
+            answer = [weight, height];
+            break;
+        }
+    }
+    return answer;
+}
